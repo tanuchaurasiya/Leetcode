6890e29db851b5790bdb7f69ca20cfda70603ebe
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2023-05-02 19:02:04 +0530

    Time: 32 ms (73.91%), Space: 16.4 MB (5.61%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,20 +1,16 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        
         def dfs(i,n):
-            nonlocal d
-            if i in d:
-                return d[i]
-            if s[i]=='0':
-                return 0 
-            res=0
-            res+=dfs(i+1,n) 
-            if i+1<n and ((s[i]=='1') or (s[i]=='2' and s[i+1] in '0123456')) :
-                res+=dfs(i+2,n) 
-            d[i]=res
-            return res 
+            d={n:1}
+            for i in range(n-1,-1,-1):
+                if s[i]=='0':
+                    d[i]=0
+                else:
+                    d[i]=d[i+1]
+                if i+1<n and ((s[i]=='1') or (s[i]=='2' and s[i+1] in '0123456')) :
+                    d[i]+=d[i+2] 
+            return d[0]
         n=len(s)
-        d={n:1}
         return dfs(0,n)
         
         
