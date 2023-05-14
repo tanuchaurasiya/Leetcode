ff7f8e7deeee99fc27e290bc099323ecf2677c83
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2023-05-14 14:40:54 +0530

    Time: 219 ms (20.15%), Space: 35.9 MB (5.30%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -1,66 +1,43 @@
 class node:
-    def __init__(self,data=0, flag=0):
-        self.data=data
+    def __init__(self):
         self.flag=0 
-        self.children={} 
+        self.children=[None]*26 
 class Trie:
     def __init__(self):
         self.root=node()
-        
-
     def insert(self, word: str) -> None: 
         temp=self.root
         n=len(word)
         for i in range(n):
-            if i==n-1:
-                if temp and word[i] in temp.children:
-                    temp=temp.children[word[i]] 
-                    temp.flag=1
-                else:
-                    newnode=node(word[i])
-                    temp.children[word[i]]=newnode
-                    newnode.flag=1
-                    
-            elif i==0:
-                if temp and word[i] in temp.children:
-                    temp=temp.children[word[i]] 
-                else:
-                    newnode=node(word[i])
-                    temp.children[word[i]]=newnode  
-                    temp=newnode
-            
-            else:
-                if temp and word[i] in temp.children:
-                    temp=temp.children[word[i]] 
-                else:
-                    newnode=node(word[i])
-                    temp.children[word[i]]=newnode
-                    temp=newnode 
+            idx=ord(word[i])-97
+            if temp.children[idx]==None:
+                temp.children[idx]=node() 
+            temp=temp.children[idx] 
+        temp.flag=1
+                
         
 
     def search(self, word: str) -> bool:
         temp=self.root
         n=len(word) 
         for i in range(n):
-            if temp  and word[i] not in temp.children:
-                return False 
-            else:
-                if i==n-1 and temp.children[word[i]].flag==1:
-                    return 1
-            temp=temp.children[word[i]] 
-        return 0
+            idx=ord(word[i])-97
+            if temp.children[idx]==None:
+                return 0
+            temp=temp.children[idx]  
+        return temp.flag;
+            
         
 
     def startsWith(self, prefix: str) -> bool:
         n=len(prefix)
         temp=self.root
-        for i in range(n): 
-            if temp and prefix[i] not in temp.children:
-                return 0 
-            else:
-                temp=temp.children[prefix[i]] 
+        for i in range(n):
+            idx=ord(prefix[i])-97
+            if temp.children[idx]==None:
+                return 0
+            temp=temp.children[idx]  
         return 1
-
 
 # Your Trie object will be instantiated and called as such:
 # obj = Trie()
