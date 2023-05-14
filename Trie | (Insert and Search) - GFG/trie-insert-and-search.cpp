a8ad7781e7d093ac437ce4f44614f7d207136e39
Tanu Chaurasiya <60622131+tanuchaurasiya@users.noreply.github.com>
2023-05-14 21:57:54 +0530

    Added solution - LeetHub

---
+++ Trie | (Insert and Search) - GFG/trie-insert-and-search.cpp
@@ -0,0 +1,128 @@
+//{ Driver Code Starts
+// Initial function template for C++
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Alphabet size (# of symbols)
+#define ALPHABET_SIZE (26)
+
+// Converts key current character into index
+// use only 'a' through 'z' and lower case
+#define CHAR_TO_INDEX(c) ((int)c - (int)'a')
+
+// trie node
+struct TrieNode {
+    struct TrieNode *children[ALPHABET_SIZE];
+
+    // isLeaf is true if the node represents
+    // end of a word
+    bool isLeaf;
+};
+
+// Returns new trie node (initialized to NULLs)
+struct TrieNode *getNode(void) {
+    struct TrieNode *pNode = NULL;
+
+    pNode = (struct TrieNode *) malloc(sizeof(struct TrieNode));
+
+    if (pNode) {
+        int i;
+
+        pNode->isLeaf = false;
+
+        for (i = 0; i < ALPHABET_SIZE; i++) pNode->children[i] = NULL;
+    }
+
+    return pNode;
+}
+
+void insert(struct TrieNode *, string);
+
+bool search(struct TrieNode *, string);
+
+// Driver
+int main() {
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    int t;
+    cin >> t;
+    while (t--) {
+        // Input keys (use only 'a' through 'z' and lower case)
+        int n;
+        cin >> n;
+        string keys[n];
+
+        for (int i = 0; i < n; i++) {
+            cin >> keys[i];
+        }
+
+        struct TrieNode *root = getNode();
+        // Construct trie
+        for (int i = 0; i < n; i++) insert(root, keys[i]);
+
+        string abc;
+
+        cin >> abc;
+        // Search for different keys
+
+        if (search(root, abc))
+            cout << "1\n";
+        else
+            cout << "0\n";
+        // char ex;
+        // cin >> ex;
+        // cout << ex << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+// User function template for C++
+
+// trie node
+/*
+struct TrieNode {
+    struct TrieNode *children[ALPHABET_SIZE];
+
+    // isLeaf is true if the node represents
+    // end of a word
+    bool isLeaf;
+};
+*/ 
+
+
+
+void insert(struct TrieNode *root, string word)
+{
+    TrieNode *temp=root;
+    int n= word.size();
+    for(int i=0;i<n;i++){
+        int idx=int(word[i])-97;
+        if (!temp->children[idx])
+            temp->children[idx]=new TrieNode();
+        temp=temp->children[idx] ;           
+    }
+        
+    temp->isLeaf=1;
+}
+    
+
+bool search(struct TrieNode *root, string word) 
+{
+    TrieNode *temp=root;
+    int n= word.size();
+    for (int i = 0; i < n; i++) {
+        int idx = int(word[i]) - 97; 
+        if (!temp->children[idx])
+            return 0;
+        temp = temp->children[idx] ;        
+        
+    }
+    if (temp->isLeaf==1)
+        return 1;
+    return 0;
+}
+
